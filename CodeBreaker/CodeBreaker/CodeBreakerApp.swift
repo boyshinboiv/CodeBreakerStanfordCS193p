8dab55752f845927c4cd8be11e3eba85c82fb73a
Oliver <oliver-mb@outlook.com>
2026-07-15 14:36:23 -0700

    New Project

---
+++ CodeBreaker/CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by Moultrie-Brown on 7/15/26.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
